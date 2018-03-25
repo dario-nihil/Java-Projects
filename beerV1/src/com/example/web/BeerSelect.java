669976d3a7a7f668209a63e6dfd5a10f2b192f52
Dario <Dario@192.168.56.1>
2018-03-25 15:19:30 +0200

    [add] enhanced with model class

--- beerV1/src/com/example/web/BeerSelect.java
+++ beerV1/src/com/example/web/BeerSelect.java
@@ -1,21 +1,31 @@
 package com.example.web;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.model.BeerExpert;
+
 public class BeerSelect extends HttpServlet {
-	
+
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		
+
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		String beerColor = request.getParameter("color");
+		BeerExpert expert = new BeerExpert();
+		List<String> be = expert.getBrands(beerColor);
 		out.println("Beer Selection Advice<br />");
-		out.println("<br />Got beer color: " + beerColor);
+		for (String aBeer : be) {
+			out.print("<br />Try: " + aBeer);
+		}
+
 	}
 
 }
